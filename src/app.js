0162bf48d552832757b451d2810cbee37bddf2d5
Valentin VW <valentinvanweydevelt@hotmail.com>
2023-09-16 03:21:00 +0200

    commit-require-bug-fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app = express();
+
+const movie = require("../movie.json");
+const serie = require("../serie.json");
+app.use(express.json());
+
+// ---- MOVIE ---- //
+
+app.get("/movie", (request, respond) => {
+  respond.json(movie);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// ---- SERIE ---- //
+
+app.get("/serie", (request, respond) => {
+  respond.json(serie);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on localhost3000");
 });
